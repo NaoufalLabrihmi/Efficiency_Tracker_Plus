a2052a34527b42a7bad6448d592a7e6446632d24
Naoufal <naoufal.labrihmi@gmail.com>
2024-07-01 17:17:35 +0100

    Home comp

---
+++ resources/js/Components/Icons/ApplicationLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ApplicationLogo({ className }) {
+  return (
+    <img
+      className={className}
+      src="/log.png"
+      alt="Application Logo"
+    />
+  );
+}
+
